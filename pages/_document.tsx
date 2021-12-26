39c1486667dbb6d65c55d8cf8d4ff0980f71fd80
joetifa2003 <joetifa2003@gmail.com>
2021-12-26 21:41:19 +0200

    fix: remove unused fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,11 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700;800&display=swap"
                         rel="stylesheet"
                     />
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@700&display=swap"
                         rel="stylesheet"
                     />
 
