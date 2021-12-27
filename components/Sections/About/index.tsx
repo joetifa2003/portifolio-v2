70af8ce7b2e174305afd5ecebc214af1f112b2ca
joetifa2003 <joetifa2003@gmail.com>
2021-12-27 22:11:09 +0200

    feat: stra[i integration

--- components/Sections/About/index.tsx
+++ components/Sections/About/index.tsx
@@ -2,40 +2,21 @@ import Chips from "components/UI/Chips";
 import Header from "components/UI/Header";
 import Section from "components/UI/Layout/Section";
 
-function About() {
+function About({ data }: any) {
     return (
         <Section className="container">
             <Header>About me</Header>
-            <p className="text-xl text-justify lg:text-3xl">
-                Hello! My name is Youssef and I enjoy creating things that live
-                on the internet. My interest in web development started back in
-                2015 when I decided to convert an android app to a website —
-                turns out hacking together a simple website taught me a lot
-                about HTML, CSS & JavaScript!
-                <br />
-                <br />
-                Fast-forward to today, and i have learned alot about web
-                development and I have worked on alot of side projects!
+            <p className="text-xl text-justify whitespace-pre-wrap lg:text-3xl">
+                {data.about}
             </p>
             <div className="mt-8 text-xl lg:text-2xl">
                 Here are a few technologies I've been working with recently
             </div>
             <div className="mt-4">
                 <Chips
-                    items={[
-                        "React",
-                        "Next js",
-                        "TypeScript",
-                        "Nest js",
-                        "Vue",
-                        "Golang",
-                        "Rust",
-                        "Python",
-                        "Dart/Flutter",
-                        "Prisma",
-                        "Postgresql",
-                        "Mongodb",
-                    ]}
+                    items={data.technologies.map(
+                        (technologie: any) => technologie.name
+                    )}
                 />
             </div>
         </Section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import About from "components/Sections/About";
 import Contact from "components/Sections/Contact";
 import Github from "components/Sections/Github";
@@ -7,21 +8,26 @@ import Navbar from "components/UI/Navbar";
 import { GetStaticProps } from "next";
 import { NextSeo } from "next-seo";
 import { Element } from "react-scroll";
+import api from "util/api";
 
 export const getStaticProps: GetStaticProps = async () => {
-    const res = await fetch(
-        "https://api.github.com/users/joetifa2003/repos?sort=updated&per_page=4"
-    );
+    const [githubResponse, about] = await Promise.all([
+        axios.get(
+            "https://api.github.com/users/joetifa2003/repos?sort=updated&per_page=4"
+        ),
+        api.get("/about-me"),
+    ]);
 
     return {
         props: {
-            githubData: await res.json(),
+            githubData: githubResponse.data,
+            about: about.data,
         },
         revalidate: 10,
     };
 };
 
-const App = ({ githubData }: { githubData: any }) => {
+const App = ({ githubData, about }: any) => {
     return (
         <>
             <NextSeo
@@ -38,7 +44,7 @@ const App = ({ githubData }: { githubData: any }) => {
             <Hero />
             <div className="py-8 space-y-8">
                 <Element name="about">
-                    <About />
+                    <About data={about} />
                 </Element>
                 <Element name="work">
                     <Work />
