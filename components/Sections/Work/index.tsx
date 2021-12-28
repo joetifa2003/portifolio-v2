2c5e1ef1e634975909903dec1ee898b31efe692a
joetifa2003 <joetifa2003@gmail.com>
2021-12-28 02:56:54 +0200

    Update index.tsx

--- components/Sections/Work/index.tsx
+++ components/Sections/Work/index.tsx
@@ -6,18 +6,20 @@ function Work({ projects }: any) {
     return (
         <Section className="container">
             <Header>Some of my work</Header>
-            {projects.map((project: any) => (
-                <ProjectCard
-                    key={project.name}
-                    name={project.name}
-                    imageProps={project.imageProps}
-                    description={project.description}
-                    technologies={project.technologies.map(
-                        (tech: any) => tech.name
-                    )}
-                    url={project.url}
-                />
-            ))}
+            <div className="space-y-8">
+                {projects.map((project: any) => (
+                    <ProjectCard
+                        key={project.name}
+                        name={project.name}
+                        imageProps={project.imageProps}
+                        description={project.description}
+                        technologies={project.technologies.map(
+                            (tech: any) => tech.name
+                        )}
+                        url={project.url}
+                    />
+                ))}
+            </div>
         </Section>
     );
 }
